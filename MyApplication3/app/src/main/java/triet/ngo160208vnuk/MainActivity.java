b967957c1eb8680336b10b5c13e2859716c7c47b
boyplaydn <triet.ngo160208@vnuk.edu.vn>
2017-12-29 09:23:05 +0700

    first

---
+++ MyApplication3/app/src/main/java/triet/ngo160208vnuk/MainActivity.java
@@ -0,0 +1,13 @@
+package triet.ngo160208vnuk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
